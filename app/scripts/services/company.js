d15575a7085c2bc44f01c5aba416d79e947b616c
Ernest Kuttubaev <2ernest.kg@gmail.com>
2016-02-14 01:32:48 +0600

    Change web service address to heroku deploy address

--- app/scripts/services/company.js
+++ app/scripts/services/company.js
@@ -1,13 +1,14 @@
+var COMPANY_REST_URL="https://company-api-testapp.herokuapp.com"
 angular.module('projectsApp')
         .factory('Company', ['$resource', function($resource) {
-            return $resource('http://localhost:8080/company/:id', {}, {
+            return $resource(COMPANY_REST_URL+'/company/:id', {}, {
                 query: {method: 'GET', params: {id: ''}, isArray: true},
                 update: {method: 'PUT'},
                 save: {method: 'POST'}
             })
         }])
         .factory('AddOwner', ['$resource', function($resource) {
-            return $resource('http://localhost:8080/company/:id/owner', {}, {
+            return $resource(COMPANY_REST_URL+'/company/:id/owner', {}, {
                 add: {method: 'PUT'}
             });
         }]);
